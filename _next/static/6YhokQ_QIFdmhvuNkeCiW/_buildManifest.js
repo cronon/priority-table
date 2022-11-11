fc0f143d64f497e482b639c0ac0735fd527af5bc
cronon <cronon12@gmail.com>
2022-11-11 23:00:34 +0400

    Updates

---
+++ _next/static/6YhokQ_QIFdmhvuNkeCiW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/88-656ea1c8908791fd.js","static/css/3bc58913f63a020b.css","static/chunks/pages/index-ef25cd90e01c2507.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
