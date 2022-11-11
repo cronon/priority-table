cbacc887985ffb0a546774ad85c08c3f4c8ad413
cronon <cronon12@gmail.com>
2022-11-11 23:51:48 +0400

    Updates

---
+++ _next/static/f_CVUgV7Fbc2ZsyShy5e5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/88-656ea1c8908791fd.js","static/css/b6dae7d8c0f53f72.css","static/chunks/pages/index-1638c16c64366008.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ndisi08GYml3i3jpjzWNQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/88-656ea1c8908791fd.js","static/css/3bc58913f63a020b.css","static/chunks/pages/index-ef25cd90e01c2507.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
